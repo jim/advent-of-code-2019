75497ae882e30101a25dc99c7f17f3e45609a69d
Jim Benton <jim@autonomousmachine.com>
2019-12-04 20:15:58 -0600

    Day 4, part 2.

--- 4/solution.rb
+++ 4/solution.rb
@@ -5,6 +5,10 @@ def double_digits?(number)
   number =~ /(\d)\1/
 end
 
+def only_double_digits?(number)
+  number.chars.slice_when { |a,b| a != b }.any? { |array| array.size == 2}
+end
+
 def only_ascending?(number)
   max = "0"
   number.chars.each do |char|
@@ -25,8 +29,21 @@ class PasswordTest < MiniTest::Test
     assert double_digits?("111")
     assert double_digits?("112")
     assert double_digits?("122")
-    assert double_digits?("1122")
+    assert double_digits?("1222")
+    assert double_digits?("11122")
     refute double_digits?("123789")
+  end
+
+  def test_only_double_digits
+    refute only_double_digits?("1")
+    refute only_double_digits?("123")
+    assert only_double_digits?("11")
+    refute only_double_digits?("111")
+    assert only_double_digits?("112")
+    assert only_double_digits?("122")
+    refute only_double_digits?("1222")
+    assert only_double_digits?("11122")
+    refute only_double_digits?("123789")
   end
 
   def test_only_ascending
@@ -44,9 +61,15 @@ class PasswordTest < MiniTest::Test
 end
 
 if MiniTest.run
-  number = (109165..576723).map(&:to_s)
-            .select { |n| only_ascending?(n) && double_digits?(n) }
-            .size
+  part1_answer = (109165..576723).map(&:to_s)
+                                 .select { |n| only_ascending?(n) && double_digits?(n) }
+                                 .size
 
-  puts number
+  puts "Part 1 answer: #{part1_answer}"
+
+  part2_answer = (109165..576723).map(&:to_s)
+                                 .select { |n| only_ascending?(n) && only_double_digits?(n) }
+                                 .size
+
+  puts "Part 1 answer: #{part2_answer}"
 end
